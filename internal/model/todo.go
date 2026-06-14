e9a1ddad5826da2d78b81b22a69b7e864c234065
Mayank Bansal <mayankbansal125@gmail.com>
2026-06-14 05:35:19 +0530

    feat: define model and storage for I/O operations

---
+++ internal/model/todo.go
@@ -0,0 +1,12 @@
+package model
+
+type Todo struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Priority *int `json:"priority,omitempty"`
+	DueDate string `json:"due_date"`
+	CompletedAt *string `json:"completed_at,omitempty"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
