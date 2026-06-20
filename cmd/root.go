33185a6952d96230aae20bc4857b15d1a906ceef
Mayank Bansal <mayankbansal125@gmail.com>
2026-06-21 02:45:23 +0530

    chore: cleanup root cmd desc and add version flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -9,23 +6,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "yak",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Version: "0.1.0",
+	Short: "A fast and minimal CLI to manage todos",
+	Long: `yak is a lightweight CLI todo manager that makes managing your daily tasks simple and fast.
+Run the help command for instructions or go through README.md to know more about CLI.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Quick start:
+  yak add "Buy groceries"           Add a todo
+  yak add "Pay rent" -p 0 -t 25-06  Add with high priority & deadline 25 June 2026
+  yak list                          List all pending todos
+  yak today                         Show today's top 3 todos
+  yak next                          Show upcoming todos by deadline
+  yak mark 1                       Mark todo #1 as done`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -33,14 +29,4 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.yak.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+func init() {}
